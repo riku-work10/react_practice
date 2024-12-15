a01ff9d269fb352853820280a5efc5db6698e62b
riku-work10 <ukir19990530@gmail.com>
2024-12-15 22:51:51 +0900

    Reactコンポーネントを作るまで完了

---
+++ src/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  const componentName = () => 'Header';
+
+  return (
+    <div className="test-wrapper">
+      このコンポーネントの名前は{componentName()}です。
+    </div>
+  );
+};
