427685cc203d4afb88c5ed5890fb6d900d120de4
riku-work10 <ukir19990530@gmail.com>
2024-12-16 12:34:38 +0900

    ライブラリを導入するまで完了

--- src/src/App.js
+++ src/src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
 // Headerコンポーネントをimportする
 import { Header } from './components/Header';
+import React from 'react';
+import { FaBeer } from 'react-icons/fa';
+
+
+class Question extends React.Component {
+  render() {
+    return <h3> Lets go for a <FaBeer />? </h3>
+  }
+}
+
 
 function App() {
   return (
